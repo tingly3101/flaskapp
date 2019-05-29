1796bb26e31688c18f3a5bc0d91a7a030e4f2a58
kritsana <tingly_3101@gmail.com>
2019-05-29 13:22:59 +0700

    fixed python

--- API.py
+++ API.py
@@ -1,21 +1,12 @@
 from flask import Flask,jsonify,request,abort
 from flask_cors import CORS,cross_origin
 
-
-
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'application/json'
 cors = CORS(app)
 
-
-
 @app.route('/' , methods=['GET','POST'])
 @cross_origin(supports_credentials=True)
 def first_function():
-
     return "Hello from flask"
 
-
-#ทดสอบการยิง Rest API ด้วย method Get Post เพื่อทดสอบการตอบสนอง
-if __name__ == '__main__':
-    app.run(debug=False,host=(''))
